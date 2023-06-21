b223c7e8d6ea235c6c785d3209eda8892738e481
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-06-21 12:25:36 +0100

    Basic structure of the dashboard

---
+++ src/Components/InfoPanel.jsx
@@ -0,0 +1,58 @@
+import React, {useState,useEffect} from "react";
+import { Card,Title, Text, Metric, Divider } from "@tremor/react";
+
+const MainDash = (props = {}) => {
+    const [infoList,setInfoList] = useState([]);
+    const [globalValues,setGlobalValues] = useState({});
+    useEffect(()=>{
+        const {csvData,headers} = props;
+        let tempList = [];
+        let g = {};
+        g["duration"] = 0;
+        g["energy"] = 0;
+        g["emissions"] = 0;
+        if(csvData.length){
+            for(let i = 0; i<csvData.length-1;++i){
+                let tempContainer = {};
+                tempContainer["name"] = csvData[i][headers["project_name"]];
+                tempContainer["country"] = csvData[i][headers["country_name"]];
+                tempContainer["cloud"] = csvData[i][headers["cloud_region"]];
+                tempContainer["cpumodel"] = csvData[i][headers["cpu_model"]];
+                tempContainer["gpumodel"] = csvData[i][headers["gpu_model"]];
+                tempList.push(tempContainer);
+                g["duration"]+=Number.parseFloat(csvData[i][headers["duration"]]).toFixed(2);
+                g["energy"]+=Number.parseFloat(csvData[i][headers["energy_consumed"]]).toFixed(5);
+                g["emissions"]+=Number.parseFloat(csvData[i][headers["emissions"]]).toFixed(5);
+            }
+        
+        }
+        setInfoList(tempList);
+        setGlobalValues(g);
+    },[props])
+    return (
+        <div className="flex flex-col justify-center">
+            <Title className="text-black align-center">Info Panel</Title>
+            <div className="flex flex-row">
+            {infoList.length &&
+                <Card className="max-w-xs mx-auto">
+                    <Text className="text-white">{`Project Name: ${infoList[0]["name"]}`}</Text>
+                    <Text className="text-white">{`Country: ${infoList[0]["country"]}`}</Text>
+                    <Text className="text-white">{`Cloud Provider: ${infoList[0]["cloud"]}`}</Text>
+                    <Text className="text-white">{`CPU : ${infoList[0]["cpumodel"]}`}</Text>
+                    <Text className="text-white">{`GPU : ${infoList[0]["gpumodel"]}`}</Text>
+                </Card>
+            }
+            {globalValues && 
+            <Card className=" mx-auto max-w-xs">
+                <Metric className="text-white">{`Duration: ${globalValues["duration"]} Seconds`}</Metric>
+                <Divider/>
+                <Metric className="text-white">{`Energy: ${globalValues["energy"]} KwH`}</Metric>
+                <Metric className="text-white">{`Emissions: ${globalValues["emissions"]} CO2 Kg`}</Metric>
+            </Card>
+            }
+            </div>
+            <Divider/>
+        </div>
+    )
+}
+export default MainDash
---
+++ src/Components/ParentContainer.jsx
@@ -0,0 +1,40 @@
+import React,{useState,useEffect} from "react";
+import MainDash from './InfoPanel';
+import CarbonDonut from './CarbonDonut';
+import { Title, Divider } from "@tremor/react";
+import * as Papa from 'papaparse';
+
+const ParentContainer = (props = {}) => {
+    const [csvFile,setCsvFile] = useState([]);
+    const [csvHeaders,setCsvHeaders] = useState({});
+    useEffect(() => {
+        fetch( './out.csv' )
+            .then( response => response.text() )
+            .then( responseText => {
+                let data = Papa.parse(responseText);
+                // Always generate the CSV file with headers
+                setCsvHeaders(data.data[0]);
+                const headers = data.data[0];
+                let temp = {};
+                for(let i = 0;i<headers.length;++i){
+                    temp[headers[i]] = i;
+                }  
+                setCsvHeaders(temp);
+                setCsvFile(data.data.slice(1));
+            })
+    }, [])
+
+return(
+    <div className="flex flex-col bg-teal-400 h-screen">
+        <Title className="text-black align-center">Carbon Emission Dashboard</Title>
+        <Divider/>
+        <MainDash
+        csvData = {csvFile}
+        headers = {csvHeaders}/>
+        <CarbonDonut
+        />
+    </div>
+)
+}
+
+export default ParentContainer;
